62fc3133d9a06fcd66539a8b27dd6aeb9957cfe3
Nikita Zhavoronkov <larkgronick@gmail.com>
2019-03-14 10:21:36 +0300

    feat: load solution of the lovetriangles task

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 /**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
+* @param preferences - an array of integers. Indices of people, whom they love
+* @returns number of love triangles
+*/
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+    // your implementation
+    var length = preferences.length; 
+    var count = 0;  
+    for (var n = 0; n<length; n++) { 
+    var a = preferences[n] - 1;
+    if (a == n) continue;
+    var b = preferences[a] - 1;
+    if (a == b) continue;
+    var c = preferences[b] - 1;
+    if (c == b) continue;
+    if (c == n) {
+    count++;
+    }
+    }
+    return count / 3;
+    }
